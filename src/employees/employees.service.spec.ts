7dc437f3198dede98f5a9eab8746561f8ae8280c
Peter karanja <peterkaranja60@gmail.com>
2023-12-13 04:00:06 +0300

    lesson 05

--- src/employees/employees.service.spec.ts
+++ src/employees/employees.service.spec.ts
@@ -1,7 +1,7 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { EmployeesService } from './employees.service';
+import { Test, TestingModule } from "@nestjs/testing";
+import { EmployeesService } from "./employees.service";
 
-describe('EmployeesService', () => {
+describe("EmployeesService", () => {
   let service: EmployeesService;
 
   beforeEach(async () => {
@@ -12,7 +12,7 @@ describe('EmployeesService', () => {
     service = module.get<EmployeesService>(EmployeesService);
   });
 
-  it('should be defined', () => {
+  it("should be defined", () => {
     expect(service).toBeDefined();
   });
 });
